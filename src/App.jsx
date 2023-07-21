3b799df033112f4141d65cf3ce7d8f0da490427e
jonytellodev <jonathantello.dev@gmail.com>
2023-07-20 23:27:38 -0300

    Se crea component con mensaje Hello World

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import HelloMessege from "./components/HelloMessege";
+
+function App() {
+  return (
+    <>
+      <HelloMessege></HelloMessege>
+    </>
+  );
+}
+
+export default App;
